f2b6fbccf219a7d07f0d1adae90b06e9b78b5b9c
PHAGUN-JAIN <omgill084@gmail.com>
2021-04-06 14:40:27 +0530

    ...

--- atm/src/atm/account.java
+++ atm/src/atm/account.java
@@ -2,10 +2,13 @@ package atm;
 
 public class account {
 	//account holder name
+	String holderName;
 	//account no.
+	int accno;
 	//balance
+	int balance;
 	//pin
-	
+	int pin;
 	// constructor
 	// methods
 }
